a97ab3cc812040329af702ad1b3c72d1b33501d0
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-30 18:38:53 +1000

    Implemented the snapshot testing using jest-serializer

--- Web/client/src/views/Home/spec.js
+++ Web/client/src/views/Home/spec.js
@@ -3,9 +3,8 @@ import Home from './Index.vue'
 
 describe('Home component', () => {
   it('renders test message', () => {
-    const msg = 'This is the home page'
     const wrapper = shallowMount(Home)
 
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
