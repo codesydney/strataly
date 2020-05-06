8f2533107397e944ba7d48433e39812e61af9394
Prashanga <prashanga@gmail.com>
2020-05-06 20:15:37 +1000

    WEB-First login form: without validation

--- Web/client/src/views/Home/Index.vue
+++ Web/client/src/views/Home/Index.vue
@@ -19,17 +19,49 @@ export default {
 </script>
 
 <template>
-  <div class="home">
-    <div v-if="isMobile" class="top-empty-overlay">
+  <div>
+    <div class="top-empty-overlay d-sm-none">
       <div class="container top-empty-div">
         <img class="logo" :src="logo" />
       </div>
 
-      <img v-if="isMobile" :src="skyline" class="skyline-image" />
+      <img :src="skyline" class="skyline-image" />
     </div>
 
-    <h1>Store {{ isMobile }}</h1>
-
-    <p v-if="isMobile">I'm only visible on Mobile phones ;)</p>
+    <v-container>
+      <v-form>
+        <v-container>
+          <!-- <v-row justify="center">
+            <p class="headline">Please Login</p>
+          </v-row> -->
+          <v-row justify="center">
+            <v-col cols="10" md="4">
+              <v-text-field
+                label="Email"
+                required
+                clearable
+                outlined
+                :dark="isMobile"
+              ></v-text-field>
+            </v-col> </v-row
+          ><v-row justify="center" class="my-n6">
+            <v-col cols="10" md="4">
+              <v-text-field
+                label="Password"
+                required
+                clearable
+                outlined
+                :dark="isMobile"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row justify="center">
+            <v-col cols="10" md="4">
+              <v-btn block @click="login" large color="success">Login</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-container>
   </div>
 </template>
