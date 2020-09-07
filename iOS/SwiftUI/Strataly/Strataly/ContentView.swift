166ae82f71e96bb9268ed0922db7d185fdd9fcc1
Gus Eulalio <guga.emc@gmail.com>
2020-09-07 13:00:43 +1000

    Added SwiftUI version

---
+++ iOS/SwiftUI/Strataly/Strataly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Strataly
+//
+//  Created by Gustavo E M Cabral on 7/9/20.
+//  Copyright © 2020 Gustavo Eulalio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
