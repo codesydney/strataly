1ac9b73fff9e02c6287f6df5bfc2acfb095443f1
Gus Eulalio <guga.emc@gmail.com>
2020-10-18 19:05:27 +1100

    Recreating iOS app in SwiftUI. Login screen basic layout done.

---
+++ iOS/SwiftUI/Strataly/Strataly/StratalyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StratalyApp.swift
+//  Strataly
+//
+//  Created by Gustavo E M Cabral on 18/10/20.
+//
+
+import SwiftUI
+
+@main
+struct StratalyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
